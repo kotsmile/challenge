08a00206d7a506b53c321b08ecb2008bece9c70d
Henry G <henry@chanity.io>
2026-09-03 19:17:57 +0300

    s

--- leetcode/327/main.go
+++ leetcode/327/main.go
@@ -1,72 +1,52 @@
 // Package leetcode327 https://leetcode.com/problems/count-of-range-sum/
 package leetcode327
 
-import "fmt"
-
 func countRangeSum(nums []int, lower, upper int) int {
-	fmt.Printf("nums=%v lower=%d upper=%d\n", nums, lower, upper)
 	numsPrefix := make([]int, len(nums)+1)
 	for i := range len(nums) {
 		numsPrefix[i+1] = nums[i] + numsPrefix[i]
 	}
 
-	return merge(numsPrefix, lower, upper, 0)
+	return merge(numsPrefix, lower, upper)
 }
 
-func merge(nums []int, lower, upper int, depth int) int {
-	prefix := ""
-	for range depth {
-		prefix = prefix + "\t"
-	}
-	fmt.Printf("%snums=%v\n", prefix, nums)
-
+func merge(nums []int, lower, upper int) int {
 	count := 0
 	if len(nums) <= 1 {
 		return count
 	}
 
-	l, r := nums[0:len(nums)/2], nums[len(nums)/2:]
+	left, right := nums[0:len(nums)/2], nums[len(nums)/2:]
 
-	count = merge(l, lower, upper, depth+1) + merge(r, lower, upper, depth+1)
-	newNums := make([]int, len(nums))
+	count = merge(left, lower, upper) + merge(right, lower, upper)
+	var result []int
 
 	i, j := 0, 0
-	for k := range len(nums) {
-		fmt.Printf("%si=%d j=%d k=%d count=%d\n", prefix, i, j, k, count)
+	for i < len(left) && j < len(right) {
 
-		if j >= len(r) || i >= len(l) {
-			break
-		}
-
-		if lower <= r[j]-l[i] && r[j]-l[i] <= upper {
-			fmt.Printf("%scount i=%d j=%d\n", prefix, i, j)
+		if lower <= right[j]-left[i] && right[j]-left[i] <= upper {
 			count++
 		}
-
-		if l[i] < r[j] {
-			newNums[k] = l[i]
-
-			if i+1 >= len(l) {
-				j++
-			} else {
-				i++
-			}
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
 		} else {
-			newNums[k] = r[j]
-
-			if j+1 >= len(r) {
-				i++
-			} else {
-				j++
-			}
+			result = append(result, right[j])
+			j++
 		}
+	}
+	for i < len(left) {
+		result = append(result, left[i])
+		i++
+	}
+	for j < len(right) {
+		result = append(result, right[j])
+		j++
 	}
 
 	for i := range len(nums) {
-		nums[i] = newNums[i]
+		nums[i] = result[i]
 	}
 
-	fmt.Printf("%ssorted=%v\n", prefix, nums)
-	fmt.Printf("%scount=%d\n", prefix, count)
 	return count
 }
